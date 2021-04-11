d9014ee1a3a27b01a21fde2b636f243b677d4822
Camalzade Elvin <e.camalzade@itstep.edu.az>
2021-04-11 09:10:39 +0400

    Add project files.

---
+++ WpfLessonnnn/Car.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfLessonnnn
+{
+   public class Car
+    {
+        public string Model { get; set; }
+        public string Vendor { get; set; }
+        public int Year { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Model}  {Vendor} ";
+        }
+    }
+}
---
+++ WpfLessonnnn/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfLessonnnn
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private List<Car> GetCars()
+        {
+            return new List<Car>()
+            {
+                new Car
+                {
+                     Model="E-200",
+                     Vendor="Mercedes",
+                     Year=2011
+                },
+                new Car
+                {
+                     Model="F10",
+                     Vendor="BMW",
+                     Year=2011
+                }
+            };
+        }
+        public MainWindow()
+        {
+            InitializeComponent();
+            // prgbar.IsIndeterminate = true;
+            lstbox.ItemsSource = GetCars();
+        }
+
+        //private void RepeatButton_Click(object sender, RoutedEventArgs e)
+        //{
+        //    if (prgbar.Value > prgbar.Minimum)
+        //        prgbar.Value--;
+        //}
+
+        //private void RepeatButton_Click_1(object sender, RoutedEventArgs e)
+        //{
+        //    if(prgbar.Value<prgbar.Maximum)
+        //    prgbar.Value++;
+        //}
+    }
+}
